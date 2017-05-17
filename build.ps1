e2ed45bd71474fd33fbd67243096bff7d8b17315
Nick Randell <nick@nickrandell.com>
2017-05-17 18:49:08 +0100

    Switched to cleaner build

--- build.ps1
+++ build.ps1
@@ -1,45 +1,76 @@
-Write-Output "build: Build started"
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-$env:NUGET_XMLDOC_MODE="skip"
+[CmdletBinding(PositionalBinding = $false)]
+param(
+    [string]$BuildNumber = "dev",
+    [bool] $CreatePackages = $true,
+    [bool] $RunTests = $true,
+    [bool] $CopyLocal = $false
+)
 
-$BuildConfiguration="Release"
-$ArtifactStagingDirectory="$PSScriptRoot\artifacts"
-
-if(Test-Path $ArtifactStagingDirectory) {
-    Write-Output "build: Cleaning "
-    Remove-Item $ArtifactStagingDirectory -Force -Recurse
+if (!(Test-Path "./semver.txt")) {
+    throw "No semver exists"
 }
-Get-ChildItem -rec -Filter bin | Remove-Item -Recurse -Force
-Get-ChildItem -Recurse -Filter obj | Remove-Item -Recurse -Force
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+Write-Host "BuildNumber: $BuildNumber"
+Write-Host "CreatePackages: $CreatePackages"
+Write-Host "RunTests: $RunTests"
 
-if (($env:APPVEYOR_BUILD_VERSION -ne $NULL) -and ($suffix -eq "")) {
-    $modifiedVersion = $env:APPVEYOR_BUILD_VERSION
-} else {
+Get-ChildItem -Recurse -Filter bin | Remove-Item -Recurse
+Get-ChildItem -Recurse -Filter obj | Remove-Item -Recurse
+
+$packageOutputFolder = "$PSScriptRoot\.nupkgs"
+
+$semVer = Get-Content (Join-Path $PSScriptRoot "semver.txt")
+if ($BuildNumber -eq "dev") {
     $autoVersion = [math]::floor((New-TimeSpan $(Get-Date) $(Get-Date -month 1 -day 1 -year 2016 -hour 0 -minute 0 -second 0)).TotalMinutes * -1).ToString() + "-" + (Get-Date).ToString("ss")
-    $modifiedVersion = "1.3.0-beta-$autoVersion"
-    
+    $version = "$semVer-dev-$autoVersion"
+    $configuration = "Debug"
+}
+else {
+    $version = "$semVer$BuildNumber"
+    $configuration = "Release"
 }
 
-dotnet --version
-dotnet restore /p:Version=$modifiedVersion
-if($LASTEXITCODE -ne 0) { exit 1 }
-dotnet build --configuration ${BuildConfiguration} /p:Version=$modifiedVersion
-if($LASTEXITCODE -ne 0) { exit 1 }
-
-$projects = $(Get-ChildItem src -rec -filter *.csproj | ForEach-Object { $_.FullName })
-foreach ($project in $projects) {
-    dotnet pack --no-build --configuration ${BuildConfiguration} --output ${ArtifactStagingDirectory} /p:Version=$modifiedVersion $project
-    if($LASTEXITCODE -ne 0) { exit 1 }
+Write-Host "Restore"
+dotnet restore "/p:Version=$version"
+if ($LastExitCode -ne 0) { 
+    Write-Host "Error with restore, aborting build." -Foreground "Red"
+    Exit 1
 }
 
-if (($env:APPVEYOR_BUILD_VERSION -ne $NULL) -and ($suffix -eq "")) {
-    Write-Host "Built ok"
-} else {
-    Write-Host "Copying to local publish directory"
-    Copy-Item $ArtifactStagingDirectory\*.nupkg c:\users\nick\source\packages
-
+if ($RunTests) {
+    Write-Host "Running tests"
+    Get-ChildItem  -Recurse "test\*.csproj" |
+        ForEach-Object {
+        & dotnet test $_
+        if ($LastExitCode -ne 0) { 
+            Write-Host "Error with test, aborting build." -Foreground "Red"
+            Exit 1
+        }
+    }
 }
+
+Write-Host "Building"
+dotnet build -c $configuration "/p:Version=$version"
+if ($LastExitCode -ne 0) { 
+    Write-Host "Error with build, aborting build." -Foreground "Red"
+    Exit 1
+}
+
+
+
+if ($CreatePackages) {
+    Write-Host "Packing"
+    mkdir -Force $packageOutputFolder | Out-Null
+
+    Get-ChildItem $packageOutputFolder | Remove-Item
+    dotnet pack --no-build --output $packageOutputFolder -c $configuration "/p:Version=$version" 
+    if ($LastExitCode -ne 0) { 
+        Write-Host "Error with pack, aborting build." -Foreground "Red"
+        Exit 1
+    }
+    if ($CopyLocal) {
+        Copy-Item -Path "$packageOutputFolder\*.nupkg" -Destination "$env:HOME\Source\Packages"
+    }
+}
+Write-Host "Complete"
+
