92be49d60840116385a5f3dc0be16f13748d667d
Nick Randell <nick@nickrandell.com>
2017-09-04 12:43:13 +0100

    Update build

--- build.ps1
+++ build.ps1
@@ -1,59 +1,42 @@
 [CmdletBinding(PositionalBinding = $false)]
 param(
-    [string]$BuildNumber = "dev",
     [bool] $CreatePackages = $true,
     [bool] $RunTests = $true,
-    [bool] $CopyLocal = $false
+    [bool] $CopyLocal = $false,
+    [string] $Configuration = "Release"
 )
 
-if (!(Test-Path "./semver.txt")) {
-    throw "No semver exists"
-}
-
-Write-Host "BuildNumber: $BuildNumber"
-Write-Host "CreatePackages: $CreatePackages"
-Write-Host "RunTests: $RunTests"
-
-Get-ChildItem -Recurse -Filter bin | Remove-Item -Recurse
-Get-ChildItem -Recurse -Filter obj | Remove-Item -Recurse
+Push-Location $PSScriptRoot
 
 $packageOutputFolder = "$PSScriptRoot\.nupkgs"
 
-$semVer = Get-Content (Join-Path $PSScriptRoot "semver.txt")
-if ($semVer.Contains("-")) {
-    if (-not $semVer.EndsWith("-")) {
-        throw "Semver with a dash should end in dash"
-    }
-}
-else {
-    if (-not $semVer.EndsWith(".")) {
-        throw "Semver should end with a dot"
-    }
-}
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$autoVersion = [math]::floor((New-TimeSpan $(Get-Date) $(Get-Date -month 1 -day 1 -year 2016 -hour 0 -minute 0 -second 0)).TotalMinutes * -1).ToString() + "-" + (Get-Date).ToString("ss")
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local-$autoVersion" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$packSuffix = @{ $true=""; $false="--version-suffix=$suffix"}[$suffix -eq ""]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
-if ($BuildNumber -eq "dev") {
-    $semVer = $semVer.Substring(0, $semVer.Length - 1)
-    $autoVersion = [math]::floor((New-TimeSpan $(Get-Date) $(Get-Date -month 1 -day 1 -year 2016 -hour 0 -minute 0 -second 0)).TotalMinutes * -1).ToString() + "-" + (Get-Date).ToString("ss")
-    $version = "$semVer-dev-$autoVersion"
-    $configuration = "Debug"
-}
-else {
-    $version = "$semVer$BuildNumber"
-    $configuration = "Release"
-}
+Write-Host "build: Package version suffix is $suffix"
+Write-Host "build: Build version suffix is $buildSuffix" 
+Write-Host "build: Configuration = $Configuration"
 
-Write-Host "Restore"
-dotnet restore "/p:Version=$version"
+Write-Host "Cleaning anything old"
+dotnet clean -c $Configuration
+
+Write-Host "Building"
+dotnet build -c $Configuration --version-suffix=$buildSuffix
 if ($LastExitCode -ne 0) { 
-    Write-Host "Error with restore, aborting build." -Foreground "Red"
+    Write-Host "Error with build, aborting build." -Foreground "Red"
     Exit 1
 }
 
 if ($RunTests) {
     Write-Host "Running tests"
     Get-ChildItem  -Recurse "test\*.csproj" |
-        ForEach-Object {
-        & dotnet test $_
+    ForEach-Object {
+        & dotnet test -c $Configuration $_
         if ($LastExitCode -ne 0) { 
             Write-Host "Error with test, aborting build." -Foreground "Red"
             Exit 1
@@ -61,21 +44,12 @@ if ($RunTests) {
     }
 }
 
-Write-Host "Building"
-dotnet build -c $configuration "/p:Version=$version"
-if ($LastExitCode -ne 0) { 
-    Write-Host "Error with build, aborting build." -Foreground "Red"
-    Exit 1
-}
-
-
-
 if ($CreatePackages) {
     Write-Host "Packing"
     mkdir -Force $packageOutputFolder | Out-Null
 
     Get-ChildItem $packageOutputFolder | Remove-Item
-    dotnet pack --no-build --output $packageOutputFolder -c $configuration "/p:Version=$version" 
+    dotnet pack --output $packageOutputFolder -c $Configuration --include-symbols --no-build $packSuffix
     if ($LastExitCode -ne 0) { 
         Write-Host "Error with pack, aborting build." -Foreground "Red"
         Exit 1
@@ -86,3 +60,5 @@ if ($CreatePackages) {
 }
 Write-Host "Complete"
 
+
+Pop-Location
