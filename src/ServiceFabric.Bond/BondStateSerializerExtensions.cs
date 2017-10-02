b85c105e00bdacdb5b50058ae40171c63c8984a9
Nick Randell <nick@nickrandell.com>
2017-10-02 12:05:03 +0100

    Moved initialization callback adapter

---
+++ src/ServiceFabric.Bond/BondStateSerializerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ServiceFabric.Utilities;
+using System;
+
+namespace ServiceFabric.Bond
+{
+    public static class BondStateSerializerExtensions
+    {
+        [Obsolete("Uses obsolete state serializer", false)]
+        public static void RegisterBondStateSerializer<T>(this InitializationCallbackAdapter adapter) =>
+            adapter.RegisterStateSerializer<T>(new BondStateSerializer<T>());
+    }
+}
