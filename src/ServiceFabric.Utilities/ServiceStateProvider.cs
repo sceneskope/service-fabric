609bb13db5965770d56baee1cb5d14237438a007
Nick Randell <nick.randell@sceneskope.com>
2018-08-09 15:38:54 +0100

    Added a service state provider

---
+++ src/ServiceFabric.Utilities/ServiceStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Fabric;
+using System.Threading.Tasks;
+
+namespace ServiceFabric.Utilities
+{
+    public class ServiceStateProvider<TState>
+        where TState : class
+    {
+        private TState _activeState;
+
+        public TState Active
+        {
+            get
+            {
+                var active = _activeState;
+                if (active == null)
+                {
+                    throw new FabricTransientException("Not ready");
+                }
+                else
+                {
+                    return active;
+                }
+            }
+        }
+
+        public async Task<IDisposable> CreateAsync(Func<Task<TState>> asyncCreator)
+        {
+            var state = await asyncCreator().ConfigureAwait(false);
+            return new Disposer(this, state);
+        }
+
+        private class Disposer : IDisposable
+        {
+            private readonly ServiceStateProvider<TState> _serviceStateProvider;
+
+            public void Dispose()
+            {
+                _serviceStateProvider._activeState = default;
+            }
+
+            public Disposer(ServiceStateProvider<TState> serviceStateProvider, TState state)
+            {
+                _serviceStateProvider = serviceStateProvider;
+                serviceStateProvider._activeState = state;
+            }
+        }
+    }
+}
